22b8b6555e85b92dcaa45edd77e15f2d9d9754c0
DaNiK2 <daniel.karamov@gmail.com>
2026-09-20 13:50:19 +0300

    Updates work of linux.py

--- linux.py
+++ linux.py
@@ -1,6 +1,6 @@
 import os
 
-def linux_parser(): #this func return cpu_info in linux systems 
+def linux_processor():
     processor = os.path.join("/", "proc", "cpuinfo")
     core_num = 0
     model = ""
@@ -13,3 +13,27 @@ def linux_parser(): #this func return cpu_info in linux systems
     
     return [model, core_num]
     
+def linux_uptime():
+    net = os.path.join("/", "proc", "uptime")
+
+    with open(net, "r", encoding="utf-8") as f:
+        uptime = f.readline().split(" ")[0]
+
+    return uptime
+
+def linux_memory():
+    mem = os.path.join("/", "proc", "meminfo")
+    Total_mem = 0
+    Free_mem = 0
+    Available_mem = 0
+    with open(mem, "r", encoding="utf-8") as f:
+        for l in f:
+            if "Total" in l and Total_mem == 0:
+                Total_mem = l.split(":")[1].strip()
+            elif "Free" in l and Free_mem == 0:
+                Free_mem = l.split(":")[1].strip()
+            elif "Available" in l and Available_mem == 0:
+                Available_mem = l.split(":")[1].strip()
+    return [Total_mem, Free_mem, Available_mem]
+                
+
--- main.py
+++ main.py
@@ -18,9 +18,15 @@ if parametrs["OS"] == "Windows":
 
 elif parametrs["OS"] == "Linux":
     parametrs["Processes"] = subprocess.check_output("ps", text = True)
-    Cpu_name, core_num = linux.linux_parser()
+    Cpu_name, core_num = linux.linux_processor()
     parametrs["Cpu_name"] = Cpu_name
     parametrs["Number of CPU cores"] = core_num
+    parametrs["Uptime"] = linux.linux_uptime()
+    Total_mem, Free_mem, Available_mem = linux.linux_memory()
+    parametrs["Total memory"] = Total_mem
+    parametrs["Free memory"] = Free_mem
+    parametrs["Available memory"] = Available_mem
+
 
 
 
