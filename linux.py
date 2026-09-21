5f639173199677fdaa73146eaa52516c1461a845
DaNiK2 <daniel.karamov@gmail.com>
2026-09-21 17:12:00 +0300

    Rewrite linux logic in main.py Add network data in linux

--- linux.py
+++ linux.py
@@ -1,4 +1,14 @@
 import os
+import subprocess
+
+VPN_TYPES = {
+    "tun",          
+    "tap",          
+    "wireguard",    
+    "vpn",          
+    "ppp",          
+    "ipip", "gre",  
+}
 
 def linux_processor():
     processor = os.path.join("/", "proc", "cpuinfo")
@@ -35,5 +45,32 @@ def linux_memory():
             elif "Available" in l and Available_mem == 0:
                 Available_mem = l.split(":")[1].strip()
     return [Total_mem, Free_mem, Available_mem]
-                
 
+def get_network():
+    data = subprocess.run(["nmcli", "-t", "connection", "show"], capture_output=True, text=True)
+    net_name=""
+    vpn = False
+    for l in data.stdout.splitlines():
+        collected_data = l.split(":")
+        if len(collected_data[-1]) != 0 and len(net_name) == 0:
+            net_name = collected_data[0]
+        if collected_data[-2] in VPN_TYPES:
+            vpn=True
+    return [net_name, vpn]
+            
+
+def data_collector():
+    collected_data = {}
+    collected_data["Processes"] = subprocess.check_output("ps", text = True)
+    Cpu_name, core_num = linux_processor()
+    collected_data["Cpu_name"] = Cpu_name
+    collected_data["Number of CPU cores"] = core_num
+    collected_data["Uptime"] = linux_uptime()
+    Total_mem, Free_mem, Available_mem = linux_memory()
+    collected_data["Total memory"] = Total_mem
+    collected_data["Free memory"] = Free_mem
+    collected_data["Available memory"] = Available_mem
+    Net_name, vpn = get_network()
+    collected_data["Network name"] = Net_name
+    collected_data["VPN connections"] = vpn
+    return collected_data 
